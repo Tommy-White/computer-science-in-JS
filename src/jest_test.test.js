8eade38692361669a529038c401369e1ef6b7a44
🔥 TommY ❄️ <lihroff@icloud.com>
2018-11-24 14:12:59 +0800

    jest test

---
+++ src/jest_test.test.js
@@ -0,0 +1,16 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function printHelloWorld() {
+  return "Hello World";
+}
+
+test('adds 1 + 2 to equal 3', () => {
+
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('fn printHelloWorld', () => {
+  expect(printHelloWorld()).toEqual("Hello World");
+});
